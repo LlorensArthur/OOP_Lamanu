ed2402ea69595ce8a3c49b7ac31bdd756c537e4e
LlorensArthur <arthurllorens@hotmail.fr>
2022-01-26 12:25:35 +0100

    Added classes described in the exercice statement

---
+++ Logs.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class Logs
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private int participation;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private float tauxReussite;
+
+
+
+  #endregion
+
+
+  #region Public methods
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
